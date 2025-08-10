1f1444b53cd60959da8b3e117b4cf079a1885864
masatakaaaaaan <mstk0246@yahoo.co.jp>
2025-08-11 08:09:10 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // pages/index.js
-import React from 'react';
-
-export default function HomePage() {
-  return <h1>Hello, Vercel!</h1>;
+export default function Home() {
+  return (
+    <div>
+      {/* あなたのコンテンツ */}
+    </div>
+  );
 }
